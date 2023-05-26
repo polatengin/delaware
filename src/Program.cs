3f77a2a97601345d75fe9287b9e5dc38ed6e0723
Engin Polat <polatengin@hotmail.com>
2023-05-25 18:24:38 -0700

    creating entrypoint of the application

--- src/Program.cs
+++ src/Program.cs
@@ -9,4 +9,22 @@ using Azure.Search.Documents.Models;
 public class Program
 {
   const string indexName = "route";
+  const string key = "<AzureSearchAPIKey>";
+
+  static Uri endpoint = new Uri("https://<azure_search>.search.windows.net");
+
+  static AzureKeyCredential credential = new AzureKeyCredential(key);
+  static SearchIndexClient indexClient = new SearchIndexClient(endpoint, credential);
+  static SearchClient searchClient = new SearchClient(endpoint, indexName, credential);
+
+  public static void Main(string[] args)
+  {
+    MainAsync(args).GetAwaiter().GetResult();
+
+    Console.WriteLine("done!...");
+  }
+
+  private static async Task MainAsync(string[] args)
+  {
+  }
 }
