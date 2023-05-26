a3e27ff6c53f8ba8cdf1e8d328d98c7cb96e2cfa
Engin Polat <polatengin@hotmail.com>
2023-05-25 18:23:27 -0700

    creating initial project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Azure.Search.Documents.Models;
+
+public class Program
+{
+  const string indexName = "route";
+}
