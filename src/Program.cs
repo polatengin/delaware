305e40e115e5393b477b129831e7f20417b6d9a3
Engin Polat <polatengin@hotmail.com>
2023-05-25 18:25:31 -0700

    adding class structures to create the index on azure search service

--- src/Program.cs
+++ src/Program.cs
@@ -28,3 +28,42 @@ public class Program
   {
   }
 }
+
+public class RouteInfo
+{
+  [SearchableField(IsKey = true, IsFilterable = true, IsSortable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)] public string Id { get; set; } = string.Empty;
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)] public string CarrierId { get; set; } = string.Empty;
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)] public string CarrierCompanyTitle { get; set; } = string.Empty; public double? CarrierReviewRate { get; set; }
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)] public string Name { get; set; } = string.Empty;
+  [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)] public string DepartureCountry { get; set; } = string.Empty;
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)] public string ArrivalCountry { get; set; } = string.Empty;
+  [JsonPropertyName("departureCityList")] public List<DepartureCity> DepartureCityList { get; set; } = new List<DepartureCity>();
+  [JsonPropertyName("arrivalCityList")] public List<ArrivalCity> ArrivalCityList { get; set; } = new List<ArrivalCity>();
+  [JsonPropertyName("vehicleList")] public List<Vehicle> VehicleList { get; set; } = new List<Vehicle>();
+}
+
+public class DepartureCity
+{
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  public string Name { get; set; } = string.Empty;
+}
+
+public class ArrivalCity
+{
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  public string Name { get; set; } = string.Empty;
+}
+
+public class Vehicle
+{
+  public string PriceId { get; set; } = string.Empty;
+  public string VehicleId { get; set; } = string.Empty;
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  public string VehicleTypeId { get; set; } = string.Empty;
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  public string BaggageTypeId { get; set; } = string.Empty;
+  public int? CurrencyForEntireVehicle { get; set; }
+  public int? PriceForEntireVehicle { get; set; }
+  public int? CurrencyForOneDesi { get; set; }
+  public int? PriceForOneDesi { get; set; }
+}
