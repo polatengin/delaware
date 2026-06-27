7590cc125434f3bd052c282465c514edc7f56d66
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-06-27 07:48:06 -0700

    add search endpoint export to Azure Search service creation script

--- create-azure-search.sh
+++ create-azure-search.sh
@@ -5,6 +5,7 @@ RANDOM_SUFFIX="${RANDOM}"
 RESOURCE_GROUP="rg-delaware-${RANDOM_SUFFIX}"
 LOCATION="westus"
 SEARCH_SERVICE_NAME="search-delaware-${RANDOM_SUFFIX}"
+SEARCH_ENDPOINT="https://${SEARCH_SERVICE_NAME}.search.windows.net"
 
 az group create \
   --name "$RESOURCE_GROUP" \
@@ -18,4 +19,14 @@ az search service create \
   --partition-count 1 \
   --replica-count 1
 
-echo "Search endpoint: https://${SEARCH_SERVICE_NAME}.search.windows.net"
+SEARCH_ADMIN_KEY=$(az search admin-key show \
+  --service-name "$SEARCH_SERVICE_NAME" \
+  --resource-group "$RESOURCE_GROUP" \
+  --query primaryKey \
+  --output tsv)
+
+export DELAWARE_AZURE_RESOURCE_GROUP="$RESOURCE_GROUP"
+export DELAWARE_AZURE_LOCATION="$LOCATION"
+export DELAWARE_AZURE_SEARCH_SERVICE_NAME="$SEARCH_SERVICE_NAME"
+export DELAWARE_AZURE_SEARCH_ENDPOINT="$SEARCH_ENDPOINT"
+export DELAWARE_AZURE_SEARCH_ADMIN_KEY="$SEARCH_ADMIN_KEY"
