2bb2b8fb6bf58f6b9d2212deea3e051ad2b76055
Engin Polat <118744+polatengin@users.noreply.github.com>
2026-07-03 06:11:32 -0700

    add: enhance search functionality with additional queries and normalization for Turkish characters

--- src/Program.cs
+++ src/Program.cs
@@ -29,6 +29,22 @@ public class Program
     await SendSearchRequest("tur");
 
     Console.WriteLine("`tur` search done!...");
+
+    await SendSearchRequest("ist");
+
+    Console.WriteLine("`ist` search done!...");
+
+    await SendSearchRequest("Izm");
+
+    Console.WriteLine("`Izm` search done!...");
+
+    await SendSearchRequest("İzm");
+
+    Console.WriteLine("`İzm` search done!...");
+
+    await SendSearchRequest("donus");
+
+    Console.WriteLine("`donus` search done!...");
   }
 
   private static string GetRequiredEnvironmentVariable(string name)
@@ -75,7 +91,20 @@ public class Program
 
   private static async Task<string> NormalizeSearchTextAsync(string text)
   {
-    return text.Replace("-", "");
+    return text.Replace("-", "")
+               .Replace("İ", "I")
+               .Replace("ı", "i")
+               .Replace("Ş", "S")
+               .Replace("ş", "s")
+               .Replace("Ğ", "G")
+               .Replace("ğ", "g")
+               .Replace("Ç", "C")
+               .Replace("ç", "c")
+               .Replace("Ü", "U")
+               .Replace("ü", "u")
+               .Replace("Ö", "O")
+               .Replace("ö", "o")
+               .Replace("  ", " ");
   }
 
   private static async Task<SearchResults<RouteInfo>> SendSearchRequest(string text)
@@ -92,19 +121,19 @@ public class Program
 
 public class RouteInfo
 {
-  [SearchableField(IsKey = true, IsFilterable = true, IsSortable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsKey = true, IsFilterable = true, IsSortable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string Id { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string CarrierId { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string CarrierCompanyTitle { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string Name { get; set; } = string.Empty;
-  [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string Description { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string DepartureCountry { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string ArrivalCountry { get; set; } = string.Empty;
   [JsonPropertyName("departureCityList")]
   public List<DepartureCity> DepartureCityList { get; set; } = new List<DepartureCity>();
@@ -117,13 +146,13 @@ public class RouteInfo
 
 public class DepartureCity
 {
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string Name { get; set; } = string.Empty;
 }
 
 public class ArrivalCity
 {
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string Name { get; set; } = string.Empty;
 }
 
@@ -131,9 +160,9 @@ public class Vehicle
 {
   public string PriceId { get; set; } = string.Empty;
   public string VehicleId { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string VehicleTypeId { get; set; } = string.Empty;
-  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.TrMicrosoft)]
+  [SearchableField(IsFilterable = true, AnalyzerName = LexicalAnalyzerName.Values.SrLatinMicrosoft)]
   public string BaggageTypeId { get; set; } = string.Empty;
   public int? CurrencyForEntireVehicle { get; set; }
   public int? PriceForEntireVehicle { get; set; }
